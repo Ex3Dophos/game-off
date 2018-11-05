c55ac58bd493ee627119997451d3a9f67d7ae2e9
Devon <bauerd007@gmail.com>
2018-11-04 20:31:06 -0600

    [ #12 ] - running animation working

--- characters/player/Player.gd
+++ characters/player/Player.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 # This is a demo showing how KinematicBody2D
 # move_and_slide works.
 enum MoveDirection { UP, DOWN, LEFT, RIGHT, NONE }
-
+var move_directions = ["right", "down_right", "down", "down_left", "left", "up_left", "up", "up_right"]
 # Member variables
 const MOTION_SPEED = 160 # Pixels/second
 
@@ -12,6 +12,8 @@ slave var slave_movement = MoveDirection.NONE
 
 var destination = get_position()
 var inventory = null
+var prev_position = get_position()
+var prev_velocity = Vector2()
 
 
 
@@ -62,9 +64,13 @@ func _physics_process(delta):
 			velocity = reposition
 #		if player_position != get_position():
 #			set_position(player_position.linear_interpolate(destination,delta))
-
+		var current_velocity = get_position() - prev_position
+		
 		rset_unreliable('slave_position', position)
 		rset('slave_movement', velocity)
+		_animate(current_velocity, prev_velocity)
+		prev_position = get_position()
+		prev_velocity = current_velocity
 		_move(velocity)
 	else:
 		_move(slave_movement)
@@ -74,7 +80,27 @@ func _physics_process(delta):
 		Network.update_position(int(name), position)
 
 
+func _animate(current_velocity, prev_velocity):
+	var current_direction = direction2str(current_velocity)
+	var animation = get_node('Hero').get_node('Animation')
+	match current_direction:
+		"right":  if animation.assigned_animation != "Running E" : animation.play('Running E')
+		"down_right": if animation.assigned_animation != "Running SE" : animation.play('Running SE')
+		"down": if animation.assigned_animation != "Running S" : animation.play('Running S')
+		"down_left": if animation.assigned_animation != "Running SW" : animation.play('Running SW')
+		"left": if animation.assigned_animation != "Running W" : animation.play('Running W')
+		"up_left": if animation.assigned_animation != "Running NW" : animation.play('Running NW')
+		"up": if animation.assigned_animation != "Running N" : animation.play('Running N')
+		"up_right": if animation.assigned_animation != "Running NE" : animation.play('Running NE')
 
+func direction2str(direction):
+	var angle = direction.angle()
+	if angle < 0:
+		angle += 2 * PI
+	var index = round(angle / PI * 4)
+	if index > 7:
+		index = int(index) % 8
+	return move_directions[index]
 
 
 func _move(direction):
@@ -93,3 +119,4 @@ func _move(direction):
 			#_rifle_right()
 	direction = direction.normalized() * MOTION_SPEED
 	move_and_slide(direction)
+	
