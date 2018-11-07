f116737720a3b7ddcf03f20dfdacec536942d88c
Brian Dassow <bjdassow@us.ibm.com>
2018-11-07 12:28:27 -0600

    Update Game.gd

--- Game.gd
+++ Game.gd
@@ -1,9 +1,6 @@
 extends Node
 
 func _ready():
-	#get_tree().connect('network_peer_disconnected', self, '_on_player_disconnected')
-	#get_tree().connect('server_disconnected', self, '_on_server_disconnected')
-	
 	createLevel()
 	PlayerHandler.createPlayer(str(get_tree().get_network_unique_id()), PlayerInformation.userType, get_tree().get_network_unique_id(), true)
 	Network.sendConnectedToOtherPlayers()
@@ -19,10 +16,3 @@ func _input(event):
 func createLevel():
 	var level = preload('res://levels/dungeon/dungeon.tscn').instance()
 	add_child(level)
-	
-#func _on_player_disconnected(id):
-	#print("someone disconnected")
-	#$'/root/Game/dungeon/walls'.get_node(str(id)).queue_free()
-	
-#func _on_server_disconnected():
-	#get_tree().change_scene('res://ui/main-screen/MainScreen.tscn')
